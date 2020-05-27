59779c5bc037997aa37614a494b9f98ed09f9fed
jozef507 <jozef507@gmail.com>
2020-05-27 17:47:25 +0200

    Add files via upload

--- javafx-desktop-client/src/module-info.java
+++ javafx-desktop-client/src/module-info.java
@@ -1,6 +1,12 @@
 module BP {
-    requires javafx.fxml;
+    requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.fxml;
+
     requires java.net.http;
     requires json.simple;
     requires itextpdf;
